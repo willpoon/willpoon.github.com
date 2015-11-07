416e5b0225657acb63368380a27dcdf5adad349f
poon <31402253@qq.com>
2015-11-08 07:43:01 +0800

    commit@2015-11-08 07:43

---
+++ content/0007_benifits_of_writing.m
@@ -0,0 +1,14 @@
+Title: 写作的好处
+Date: 2015-11-07 23:12
+Modified: 2015-11-07 23:22
+Category: Life
+Tags: 生活,进步,堕落
+Slug: tv-makes-people-mediocre
+Authors: Poon
+Summary: 
+
+1.写作促进思考
+2.写作的机会成本低：没空干坏事了
+3.形成自己的风格
+4.提高写作水平
+5.鞭策自己学习
